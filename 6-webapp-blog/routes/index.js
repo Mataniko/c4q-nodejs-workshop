0827174df1d178d4f782e2e97f19deb208745f11
Mataniko <mataniko@gmail.com>
2016-06-20 22:48:48 -0400

    Update names

--- 6-webapp-blog/routes/index.js
+++ 6-webapp-blog/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Matan\'s Blog' });
+  res.render('index', { title: 'My Blog' });
 });
 
 module.exports = router;
